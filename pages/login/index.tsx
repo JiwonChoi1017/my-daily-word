448521839af8ad0902896661c2237ec2d457b587
Jiwon Choi <jiwonchoi@JiwonnoMacBook-Pro.local>
2023-01-22 22:53:11 +0900

    ユーザ登録機能実装（バリデーション周りなどはまだ）

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
 import React from "react";
 
 const LoginPage = () => {
-  return <h1>Login Page</h1>;
+  return (
+    <>
+      <h1>Login Page</h1>
+      <Link href="/sign-up">ユーザ登録</Link>
+    </>
+  );
 };
 
 export default LoginPage;
--- pages/sign-up/index.tsx
+++ pages/sign-up/index.tsx
@@ -1,7 +1,23 @@
+import SignUpForm from "@/components/sign-up/SignUpForm";
+import { authService, createUserWithEmailAndPassword } from "@/firebase-config";
+
+import { useRouter } from "next/router";
 import React from "react";
 
 const SignUpPage = () => {
-  return <h1>Sign Up Page</h1>;
+  const router = useRouter();
+  const onAddUser = async (userData: { email: string; password: string }) => {
+    const { email, password } = userData;
+    await createUserWithEmailAndPassword(authService, email, password);
+    router.push("/login");
+  };
+
+  return (
+    <>
+      <h1>ユーザ登録</h1>
+      <SignUpForm onAddUser={onAddUser} />
+    </>
+  );
 };
 
 export default SignUpPage;
