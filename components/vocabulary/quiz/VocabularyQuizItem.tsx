68c02ee4a745d72bd09b509bce57d68746233588
Jiwon Choi <jiwonchoi@JiwonnoMacBook-Pro.local>
2023-03-10 23:36:25 +0900

    クイズページ実装中

--- components/vocabulary/quiz/VocabularyQuizItem.tsx
+++ components/vocabulary/quiz/VocabularyQuizItem.tsx
@@ -1,7 +1,39 @@
 import React from "react";
 
-const VocabularyQuizItem = () => {
-    return <div></div>;
-}
+const VocabularyQuizItem: React.FC<{
+  show: boolean;
+  quizKind: "word" | "meaning";
+  currentQuizNumber: number;
+  answersList: string[][];
+  correctAnswerList: { id: string; word: string; meaning: string }[];
+}> = ({
+  show,
+  quizKind,
+  currentQuizNumber,
+  answersList,
+  correctAnswerList,
+}) => {
+  if (!show) {
+    return <></>;
+  }
+
+  const currentQuestionItem = correctAnswerList[currentQuizNumber];
+  const question =
+    quizKind === "word"
+      ? currentQuestionItem.meaning
+      : currentQuestionItem.word;
+  const answerList = answersList[currentQuizNumber];
+
+  return (
+    <div>
+      <h2>{question}</h2>
+      <ul>
+        {answerList.map((answer) => {
+          return <li key={answer}>{answer}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
 
 export default VocabularyQuizItem;
--- pages/vocabulary/list/[id]/quiz/index.tsx
+++ pages/vocabulary/list/[id]/quiz/index.tsx
@@ -6,38 +6,91 @@ import { AuthContext } from "@/context/auth/AuthProvider";
 import { get, ref } from "firebase/database";
 import { db } from "@/firebase-config";
 import { Word } from "@/types/Vocabulary";
-import VocabularyQuizList from "@/components/vocabulary/quiz/VocabularyQuizList";
+import VocabularyQuizItem from "@/components/vocabulary/quiz/VocabularyQuizItem";
+import { VOCABULARY_QUIZ_COUNT } from "@/constants/constants";
 
 const VocabularyQuizPage = () => {
   const { query } = useRouter();
   const { id } = query;
 
   const [showModal, setShowModal] = useState<boolean>(true);
-  const [wordList, setWordList] = useState<Word[]>([]);
+  const [quizKind, setQuizKind] = useState<"word" | "meaning">("word");
   const [currentQuizNumber, setCurrentQuizNumber] = useState<number>(0);
+  const [answersList, setAnswersList] = useState<string[][]>([]);
+  const [correctAnswerList, setCorrectAnswerList] = useState<
+    { id: string; word: string; meaning: string }[]
+  >([]);
 
   const { currentUser } = useContext(AuthContext);
 
-  const fetchQuizWord = () => {
+  const fetchQuizWord = async () => {
+    const wordList = await fetchAllWords();
+    const quizCount =
+      wordList.length >= VOCABULARY_QUIZ_COUNT
+        ? VOCABULARY_QUIZ_COUNT
+        : wordList.length;
+    const questionIndexList: number[] = [];
+    const answersList: string[][] = [];
+    const correctAnswerList: { id: string; word: string; meaning: string }[] =
+      [];
+
+    let questionCount = 0;
+
+    while (questionCount < quizCount) {
+      const answerIndexList: number[] = [];
+      const tempAnswerList = [];
+
+      const randomWordIndex = Math.floor(Math.random() * wordList.length);
+      if (questionIndexList.includes(randomWordIndex)) continue;
+      const { id, word, meanings } = wordList[randomWordIndex];
+      const randomMeaningIndex = Math.floor(Math.random() * meanings.length);
+      const { meaning } = meanings[randomMeaningIndex];
+
+      questionIndexList.push(randomWordIndex);
+      answerIndexList.push(randomWordIndex);
+      tempAnswerList.push(word);
+      correctAnswerList.push({ id, word, meaning });
+
+      questionCount++;
+
+      let answerCount = 0;
+      while (answerCount < 3) {
+        const randomWordIndex = Math.floor(Math.random() * wordList.length);
+        if (answerIndexList.includes(randomWordIndex)) continue;
+        const { word } = wordList[randomWordIndex];
+
+        answerIndexList.push(randomWordIndex);
+        tempAnswerList.push(word);
+
+        answerCount++;
+      }
+
+      answersList.push(shuffleArray(tempAnswerList));
+    }
+    setQuizKind("word");
+    setAnswersList(answersList);
+    setCorrectAnswerList(correctAnswerList);
     setShowModal(false);
   };
 
   const fetchQuizMeaning = () => {
     setShowModal(false);
+
+    const wordList = fetchAllWords();
   };
 
   const fetchAllWords = async () => {
-    if (!currentUser) return;
+    if (!currentUser) return [];
 
     const path = `users/${currentUser.uid}/${id}/words`;
     const wordsRef = ref(db, path);
+    const wordList: Word[] = [];
 
     await get(wordsRef)
       .then((response) => {
         return response.val();
       })
       .then((value) => {
-        const wordList: Word[] = [];
         for (const key of Object.keys(value)) {
           const word: Word = {
             id: key,
@@ -45,8 +98,16 @@ const VocabularyQuizPage = () => {
           };
           wordList.push(word);
         }
-        setWordList(wordList);
       });
+    return wordList;
+  };
+
+  const shuffleArray = (array: string[]): string[] => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
   };
 
   return (
@@ -57,9 +118,12 @@ const VocabularyQuizPage = () => {
         fetchQuizWord={fetchQuizWord}
         fetchQuizMeaning={fetchQuizMeaning}
       />
-      <VocabularyQuizList
+      <VocabularyQuizItem
         show={!showModal}
+        quizKind={quizKind}
         currentQuizNumber={currentQuizNumber}
+        answersList={answersList}
+        correctAnswerList={correctAnswerList}
       />
     </MainLayout>
   );
