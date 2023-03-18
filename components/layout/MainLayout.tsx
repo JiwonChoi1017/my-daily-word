804d0a88ecd71f39bc00a2336a4a9c4f75a952b1
Jiwon Choi <jiwonchoi@JiwonnoMacBook-Pro.local>
2023-03-18 22:07:15 +0900

    css修正中、jsDoc追加

--- components/layout/MainLayout.tsx
+++ components/layout/MainLayout.tsx
@@ -1,12 +1,25 @@
-import MainNavigation from "./MainNavigation";
+import Header from "./Header";
 import React from "react";
+import classes from "../../styles/Main.module.css";
 
-// MEMO: React.FC で定義される children はオプショナルであり、型はReact.ReactNodeである。
-const MainLayout: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
+/**
+ * メインレイアウト.
+ *
+ * @param {React.ReactNode} (任意)children.
+ * @param {boolean} showNavigation - ナビゲーションを表示するか.
+ * @returns {JSX.Element} メインレイアウト.
+ */
+const MainLayout: React.FC<{
+  // MEMO: React.FC で定義される children はオプショナルであり、型はReact.ReactNodeである。
+  children?: React.ReactNode;
+  showNavigation?: boolean;
+}> = ({ children, showNavigation = true }) => {
   return (
     <div>
-      <MainNavigation />
-      <main>{children}</main>
+      <Header showNavigation={showNavigation} />
+      <main>
+        <div className={classes.main}>{children}</div>
+      </main>
     </div>
   );
 };
