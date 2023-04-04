629553d3571015c937777794d48d2147280fae8a
Jiwon Choi <jiwonchoi@JiwonnoMacBook-Pro.local>
2023-04-04 23:12:21 +0900

    トップへボタン追加

---
+++ components/ui/ScrollToTopButton.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import classes from "./ScrollToTopButton.module.css";
+import { FaAngleUp } from "react-icons/fa";
+
+/**
+ * トップへボタン.
+ *
+ * @returns {JSX.Element} トップへボタン.
+ */
+const ScrollToTopButton = () => {
+  // 表示するか
+  const [show, setShow] = useState<boolean>(false);
+  // ボタンをトグルするイベント
+  const toggleButton = () => {
+    const boundary = 100;
+    const currentPos = window.scrollY;
+
+    setShow(boundary <= currentPos);
+  };
+
+  useEffect(() => {
+    // スクロールイベントを登録
+    window.addEventListener("scroll", toggleButton);
+    return () => {
+      window.removeEventListener("scroll", toggleButton);
+    };
+  }, []);
+
+  // トップへスクロールするイベント
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+  // クラス名
+  const className = `${show ? classes.show : classes.hide} ${
+    classes.scrollToTopButton
+  }`;
+
+  return (
+    <div className={className}>
+      <FaAngleUp className={classes.arrow} onClick={scrollToTop} />
+    </div>
+  );
+};
+
+export default ScrollToTopButton;
