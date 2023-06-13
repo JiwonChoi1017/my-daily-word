2adb001e1e40c72363671cdded955134f336b3fa
Jiwon Choi <jiwonchoi@JiwonnoMacBook-Pro.local>
2023-06-14 01:48:30 +0900

    クイズ画面のcss、文言などを修正

--- components/vocabulary/quiz/VocabularyQuizSelect.tsx
+++ components/vocabulary/quiz/VocabularyQuizSelect.tsx
@@ -1,29 +1,50 @@
 import Card from "@/components/ui/Card";
+import { DoubleButtonForQuiz } from "../../ui/Button";
 import React from "react";
-import classes from "../../../styles/Button.module.css";
-import { Button } from "../../ui/Button";
+import classes from "@/styles/VocabularyQuizSelect.module.css";
+
+/** Props. */
+interface Props {
+  /** 表示するか. */
+  show: boolean;
+  /** 単語取得イベント. */
+  fetchQuizWord: () => void;
+  /** 意味取得イベント. */
+  fetchQuizMeaning: () => void;
+}
 
 /**
  * クイズ選択.
  *
- * @param {boolean} show - 表示するか.
- * @param {function} fetchQuizWord - 単語取得イベント.
- * @param {function} fetchQuizMeaning - 意味取得イベント.
+ * @param {Props} props
  * @returns {JSX.Element} クイズ選択.
  */
-const VocabularyQuizSelect: React.FC<{
-  show: boolean;
-  fetchQuizWord: () => void;
-  fetchQuizMeaning: () => void;
-}> = ({ show, fetchQuizWord, fetchQuizMeaning }) => {
+const VocabularyQuizSelect = ({
+  show,
+  fetchQuizWord,
+  fetchQuizMeaning,
+}: Props) => {
   return show ? (
-    <Card className="textAlignCenter">
-      <div className={classes.button__wrap}>
-        <Button className="button" text={"単語"} clickHandler={fetchQuizWord} />
-        <Button
-          className="button"
-          text={"意味"}
-          clickHandler={fetchQuizMeaning}
+    <Card>
+      <div className="marginRight15">
+        <div className={classes.question}>
+          <span>クイズの形式を選択してください。</span>
+        </div>
+        <DoubleButtonForQuiz
+          button={{
+            first: {
+              className: "first",
+              text: "意味 → 単語",
+              clickHandler: fetchQuizWord,
+              note: "質問として意味が表示され、回答として単語が表示されます。",
+            },
+            second: {
+              className: "first",
+              text: "単語 → 意味",
+              clickHandler: fetchQuizMeaning,
+              note: "質問として単語が表示され、回答として意味が表示されます。",
+            },
+          }}
         />
       </div>
     </Card>
